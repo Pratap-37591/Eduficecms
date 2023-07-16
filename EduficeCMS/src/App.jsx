68d2d2923f1ad52bd903614967c14f2fc8bac3ed
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-16 22:01:15 +0530

    navigation added

--- EduficeCMS/src/App.jsx
+++ EduficeCMS/src/App.jsx
@@ -1,11 +1,53 @@
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Pricing from "./pages/Pricing/Pricing";
+import Support from "./pages/Support/Support";
+
+const Layout = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "/support",
+        element: <Support />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
     <div>
-    <Header/>
+      <Header />
+      <Navbar />
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
