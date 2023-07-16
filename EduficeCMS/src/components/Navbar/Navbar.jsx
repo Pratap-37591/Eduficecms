28e1095cad068bd9a478ae2386f4cea057b3ddd6
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-16 22:01:48 +0530

    navigation

--- EduficeCMS/src/components/Navbar/Navbar.jsx
+++ EduficeCMS/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+// import EduLogo from "../../assets/Logo.png";
+import "../Navbar/Navbar.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <BrowserRouter>
+      <nav className="nav-container">
+        <div className="left">
+          <img
+            src="https://beta.edificecms.com/builder/elements/images/uploads/project30/168422388164.png"
+            alt="EduLogo"
+          />
+        </div>
+        <div className="menu-links">
+          <ul>
+            <li>
+              <Link to="/">Home </Link>
+            </li>
+
+            <li>
+              <Link to="/pricing">Pricing </Link>
+            </li>
+
+            <li>
+              <Link to="/support">Support </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </BrowserRouter>
+  );
+};
 
 export default Navbar;
